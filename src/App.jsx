d4b9d02e09dc691dbbc4d7db8150f6a61c663d9c
Saurabh singh <saurabhsingh941576@gmail.com>
2026-03-30 18:46:26 +0530

    fix: add build config

--- src/App.jsx
+++ src/App.jsx
@@ -9,72 +9,73 @@ import MainPage from "./components/MainPage";
 import ResultPage from "./components/ResultPage";
 import ResultAnalysis from "./components/ResultAnalysis";
 import LoginConfirmation from "./components/LoginConfirmation";
-
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
 
-        <Route
-          path="/main"
-          element={
-            // <ProtectedRoutes>
-            <MainPage />
-            // </ProtectedRoutes>
-          }
-        />
-        <Route
-          path="/test"
-          element={
-            <ProtectedRoutes>
-              <TestPage />
-            </ProtectedRoutes>
-          }
-        />
-        <Route
-          path="/result-page"
-          element={
-            <ProtectedRoutes>
-              <ResultPage />
-            </ProtectedRoutes>
-          }
-        />
-        <Route
-          path="/exit"
-          element={
-            <ProtectedRoutes>
-              <ExitPage />
-            </ProtectedRoutes>
-          }
-        />
-        <Route
-          path="/result-lock"
-          element={
-            <ProtectedRoutes>
-              <ResultLock />
-            </ProtectedRoutes>
-          }
-        />
-        <Route
-          path="/result"
-          element={
-            <ProtectedRoutes>
-              <ResultAnalysis />
-            </ProtectedRoutes>
-          }
-        />
-        <Route
-          path="/confirmation"
-          element={
-            // <ProtectedRoutes>
-            <LoginConfirmation />
-            // </ProtectedRoutes>
-          }
-        />
-      </Routes>
-    </Router>
+          <Route
+            path="/main"
+            element={
+              // <ProtectedRoutes>
+              <MainPage />
+              // </ProtectedRoutes>
+            }
+          />
+          <Route
+            path="/test"
+            element={
+              <ProtectedRoutes>
+                <TestPage />
+              </ProtectedRoutes>
+            }
+          />
+          <Route
+            path="/result-page"
+            element={
+              <ProtectedRoutes>
+                <ResultPage />
+              </ProtectedRoutes>
+            }
+          />
+          <Route
+            path="/exit"
+            element={
+              <ProtectedRoutes>
+                <ExitPage />
+              </ProtectedRoutes>
+            }
+          />
+          <Route
+            path="/result-lock"
+            element={
+              <ProtectedRoutes>
+                <ResultLock />
+              </ProtectedRoutes>
+            }
+          />
+          <Route
+            path="/result"
+            element={
+              <ProtectedRoutes>
+                <ResultAnalysis />
+              </ProtectedRoutes>
+            }
+          />
+          <Route
+            path="/confirmation"
+            element={
+              // <ProtectedRoutes>
+              <LoginConfirmation />
+              // </ProtectedRoutes>
+            }
+          />
+        </Routes>
+      </Router>
+    </>
   );
 };
 
--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, use } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { BrowserRouter, useNavigate } from "react-router-dom";
 
 // Inner component that safely uses useNavigate
